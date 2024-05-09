12cdd1ec637056dc6f20c376d576f0de3027a453
Mayra Landa <mlandae16@gmail.com>
2024-05-09 15:17:01 -0700

    add webpack, eslint, prettier, and babel

---
+++ src/index.js
@@ -0,0 +1,11 @@
+var xyz = 10;
+
+function example(args = 1) {
+  if ((args = 1)) {
+    //do something
+  } else if ((args = 2)) {
+    //do something
+  } else if ((args = 1)) {
+    //do something
+  }
+}
